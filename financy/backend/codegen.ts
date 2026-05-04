70103beb3ebfb67f6b46f38518162980434203fd
Lucas Porto <lversiani@supermoney.com>
2026-05-04 10:12:23 -0300

    chore(financy-backend): configure graphql-codegen

---
+++ financy/backend/codegen.ts
@@ -0,0 +1,21 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  schema: './src/schema.graphql',
+  generates: {
+    './src/generated/graphql.ts': {
+      plugins: [
+        { add: { content: '/* eslint-disable */' } },
+        'typescript',
+        'typescript-resolvers',
+      ],
+      config: {
+        contextType: '../context#GraphQLContext',
+        useIndexSignature: true,
+        scalars: { DateTime: 'Date' },
+      },
+    },
+  },
+};
+
+export default config;
